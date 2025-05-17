b693e631e8a286b0997ee5ad0924fb4ba40c481d
Anton Oresten <antonoresten@gmail.com>
2025-05-17 16:19:45 +0200

    fix test in 1.9

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,8 +30,11 @@ using Test, Statistics, LinearAlgebra
         @test parse_shape(x, (:a, :b, ..)) == (; a = 2, b = 3)
         @test parse_shape(x, (:a, :b, :c, ..)) == (; a = 2, b = 3, c = 5)
 
-        h(x) = parse_shape(x, (:a, :b, :c)); # tuple-pattern must be const-propagated if not passed as a Val
-        @test (@inferred h(x)) == (; a = 2, b = 3, c = 5)
+        if VERSION >= v"1.10"
+            h(x) = parse_shape(x, (:a, :b, :c)); # tuple-pattern must be const-propagated if not passed as a Val
+            @test (@inferred h(x)) == (; a = 2, b = 3, c = 5)
+        end
+
         @test (@inferred parse_shape(x, Val((:a, :b, :c)))) == (; a = 2, b = 3, c = 5)
         @test (@inferred parse_shape(x, Val((:a, :b, -)))) == (; a = 2, b = 3)
         @test (@inferred parse_shape(x, Val((:a, :b, ..)))) == (; a = 2, b = 3)
