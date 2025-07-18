ddad75df54e24a495260e866c9412c8800fcfc0f
Anton Oresten <antonoresten@gmail.com>
2025-07-19 00:27:23 +0200

    allow duplicate names in parse_shape

--- src/parse_shape.jl
+++ src/parse_shape.jl
@@ -4,6 +4,8 @@
 Capture the shape of an array in a pattern by naming dimensions using `Symbol`s,
 and `-` to ignore dimensions, and `...` to ignore any number of dimensions.
 
+Duplicate symbols are allowed, but they must refer to dimensions of the same size.
+
 !!! note
     For proper type inference, the pattern needs to be passed as `Val(pattern)`
     when an ellipsis is present. This is done automatically when using [`@einops_str`](@ref).
@@ -22,15 +24,32 @@ julia> parse_shape(rand(2,3,4,5), einops"first second third fourth")
 
 julia> parse_shape(rand(2,3,4), Val((:a, :b, ..)))
 (a = 2, b = 3)
+
+julia> parse_shape(rand(2,2,4), (:a, :a, :b))  # duplicate 'a' with same size
+(a = 2, b = 4)
 ```
 """
 @generated function parse_shape(x::AbstractArray{<:Any,N}, ::Val{pattern}) where {N, pattern}
     pattern′ = replace_ellipses_parse_shape(pattern, N)
     inds = findall(x -> x isa Symbol, pattern′)
-    names = pattern′[inds]
-    allunique(names) || error("Pattern $(pattern) has duplicate elements")
+    names = pattern′[inds]    
+    unique_names = unique(names)
+    duplicate_checks = []
+    for name in unique_names
+        name_indices = findall(==(name), names)
+        if length(name_indices) > 1
+            first_idx = inds[name_indices[1]]
+            for i in 2:length(name_indices)
+                idx = inds[name_indices[i]]
+                push!(duplicate_checks, :(size(x, $first_idx) == size(x, $idx) || 
+                    error("Dimension $($(QuoteNode(name))) appears multiple times with different sizes: $(size(x, $first_idx)) and $(size(x, $idx))")))
+            end
+        end
+    end
+    unique_inds = [inds[findfirst(==(name), names)] for name in unique_names]
     quote
-        NamedTuple{$names}($(Expr(:tuple, (:(size(x, $i)) for i in inds)...)))
+        $(duplicate_checks...)
+        NamedTuple{$(Tuple(unique_names))}($(Expr(:tuple, (:(size(x, $i)) for i in unique_inds)...)))
     end
 end
 
@@ -46,7 +65,6 @@ end
 # (needs to be constant-propagated)
 function parse_shape(x::AbstractArray{<:Any,N}, pattern::Tuple{Vararg{Union{Symbol,typeof(-)}}}) where N
     names = extract(Symbol, pattern)
-    allunique(names) || error("Pattern $(pattern) has duplicate elements")
     inds = findtype(Symbol, pattern)
     shape_info = @ignore_derivatives NamedTuple{names,NTuple{length(inds),Int}}(size(x, i) for i in inds)
     return shape_info
